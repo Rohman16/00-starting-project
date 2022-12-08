c2353b44959beef8364250e2ef8b6bae1ac6d2fc
Rohman <rohman.thelearner@gmail.com>
2022-12-08 10:26:59 +0700

    add dynamic data

--- app.js
+++ app.js
@@ -10,44 +10,44 @@ srv.use(myServer.urlencoded({ extended: false }));
 srv.use(myServer.static("frontend-site"));
 
 srv.use((req, res, next) => {
-  console.log("Time:", new Date().toISOString());
-  next();
+    console.log("Time:", new Date().toISOString());
+    next();
 });
 
-srv.get("/", function (req, res) {
-  res.render("index");
+srv.get("/", function(req, res) {
+    res.render("index");
 });
 
-srv.get("/about", function (req, res) {
-  res.render("about");
+srv.get("/about", function(req, res) {
+    res.render("about");
 });
 
-srv.get("/confirm", function (req, res) {
-  res.render("confirm");
+srv.get("/confirm", function(req, res) {
+    res.render("confirm");
 });
 
-srv.get("/recommend", function (req, res) {
-  res.render("recommend");
+srv.get("/recommend", function(req, res) {
+    res.render("recommend");
 });
 
-srv.post("/recommend", function (req, res) {
-  const dataBody = req.body;
-  const filePath = path.join(__dirname, "data", "restaurant.json");
+srv.post("/recommend", function(req, res) {
+    const dataBody = req.body;
+    const filePath = path.join(__dirname, "data", "restaurant.json");
 
-  const dataRestaurant = fs.readFileSync(filePath);
-  const dataRestaurantNew = JSON.parse(dataRestaurant);
-  dataRestaurantNew.push(dataBody);
+    const dataRestaurant = fs.readFileSync(filePath);
+    const dataRestaurantNew = JSON.parse(dataRestaurant);
+    dataRestaurantNew.push(dataBody);
 
-  fs.writeFileSync(filePath, JSON.stringify(dataRestaurantNew));
+    fs.writeFileSync(filePath, JSON.stringify(dataRestaurantNew));
 
-  res.redirect("/confirm");
+    res.redirect("/confirm");
 });
 
-srv.get("/restaurants", function (req, res) {
-  const filePath = path.join(__dirname, "data", "restaurant.json");
+srv.get("/restaurants", function(req, res) {
+    const filePath = path.join(__dirname, "data", "restaurant.json");
 
-  const dataRestaurant = fs.readFileSync(filePath);
-  const dataRestaurantNew = JSON.parse(dataRestaurant);
-  res.render("restaurants", { numberRestaurant: dataRestaurantNew.length });
+    const dataRestaurant = fs.readFileSync(filePath);
+    const dataRestaurantNew = JSON.parse(dataRestaurant);
+    res.render("restaurants", { numberRestaurant: dataRestaurantNew.length, allRestaurant: dataRestaurantNew });
 });
 srv.listen(3000);
