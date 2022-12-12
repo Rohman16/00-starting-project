926a163f174ab137a183c15f2b7567c71dd25f64
rohman <rohman.thelearner@gmail.com>
2022-12-13 06:53:04 +0700

    Continue

--- views/util/router/rest-list.js
+++ views/util/router/rest-list.js
@@ -1,73 +1,77 @@
 const myExpress = require("express");
+const srv = myExpress();
 const uuid = require("uuid");
 const myRouter = myExpress.Router();
 const {
-    getRestaurantDt,
-    writeRestaurantData,
+  getRestaurantDt,
+  writeRestaurantData,
 } = require("../getDataRestaurant");
 
-myRouter.get("/confirm", function(req, res) {
-    res.render("confirm");
+srv.use(myExpress.urlencoded({ extended: false }));
+
+myRouter.get("/confirm", function (req, res) {
+  res.render("confirm");
 });
 
-myRouter.get("/recommend", function(req, res) {
-    res.render("recommend");
+myRouter.get("/recommend", function (req, res) {
+  res.render("recommend");
 });
 
-myRouter.post("/recommend", function(req, res) {
-    const dataBody = req.body;
-    dataBody.id = uuid.v4();
-    const myRestaurant = getRestaurantDt();
-    console.log(myRestaurant);
-    myRestaurant.push(dataBody);
+myRouter.post("/recommend", function (req, res) {
+  const dataBody = req.body;
+  dataBody.id = uuid.v4();
+  const myRestaurant = getRestaurantDt();
+  console.log(myRestaurant);
+  myRestaurant.push(dataBody);
 
-    writeRestaurantData(myRestaurant);
+  writeRestaurantData(myRestaurant);
 
-    res.redirect("/confirm");
+  res.redirect("/confirm");
 });
 
-myRouter.get("/restaurants", function(req, res) {
-    // let orderingStatus = req.query.sort;
-    let initStat = "asc"
-    const dataRestaurantNew = getRestaurantDt();
+myRouter.get("/restaurants", function (req, res) {
+  let initStat = "asc";
+  let orderingStatus = req.query.sort;
+  console.log(orderingStatus);
+  const dataRestaurantNew = getRestaurantDt();
 
-    dataRestaurantNew.sort(function(resA, resB) {
-        if (resA.name < resB.name) {
-            return 1;
-        }
-        return -1;
-    });
-
-    res.render("restaurants", {
-        numberRestaurant: dataRestaurantNew.length,
-        allRestaurant: dataRestaurantNew,
-        myStat: initStat
-    });
-});
-
-myRouter.get("/restaurants/:id", function(req, res) {
-    const restId = req.params.id;
-
-    const dataRestaurantNew = getRestaurantDt();
-    for (const myRest of dataRestaurantNew) {
-        if (myRest.id === restId)
-            return res.render("rest-details", { rest: myRest });
+  dataRestaurantNew.sort(function (resA, resB) {
+    if (resA.name > resB.name) {
+      return 1;
     }
-    res.status(404).render("404");
+    return -1;
+  });
+
+  res.render("restaurants", {
+    numberRestaurant: dataRestaurantNew.length,
+    allRestaurant: dataRestaurantNew,
+    myStat: initStat,
+  });
+});
+
+myRouter.get("/restaurants/:id", function (req, res) {
+  const restId = req.params.id;
+
+  const dataRestaurantNew = getRestaurantDt();
+  for (const myRest of dataRestaurantNew) {
+    if (myRest.id === restId)
+      return res.render("rest-details", { rest: myRest });
+  }
+  res.status(404).render("404");
 });
 
 function sortAsc(a, b) {
-    if (a > b) {
-        return 1;
-    }
-    return -1;
+  if (a > b) {
+    return 1;
+  }
+  return -1;
 }
 
 function sortDesc(a, b) {
-    if (a < b) {
-        return 1;
-    }
-    return -1;
+  if (a < b) {
+    return 1;
+  }
+  return -1;
 }
 
 module.exports = myRouter;
